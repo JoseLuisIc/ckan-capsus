fdca1010f851028780fb5e248c0b5154a3bfb783
root <root@instance-1.c.ckan-oskari.internal>
2020-11-22 22:06:01 -0600

    Primer Commit

---
+++ bin/pycsw-admin.py
@@ -0,0 +1,6 @@
+#!/usr/lib/ckan/ckan/bin/python2
+# EASY-INSTALL-DEV-SCRIPT: 'pycsw==2.4.2','pycsw-admin.py'
+__requires__ = 'pycsw==2.4.2'
+__import__('pkg_resources').require('pycsw==2.4.2')
+__file__ = '/usr/lib/ckan/ckan/src/pycsw/bin/pycsw-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
